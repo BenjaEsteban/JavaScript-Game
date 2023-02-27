91848f09e65769f4760ed85e3b50a7476e8f02ee
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-02-26 22:06:58 -0300

    Creación de función que detecte las teclas del usuario

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('#game')
 const game = canvas.getContext('2d')
+const btnUp = document.querySelector('#up')
+const btnLeft = document.querySelector('#left')
+const btnRight = document.querySelector('#right')
+const btnDown = document.querySelector('#down')
 
 window.addEventListener('load', setCanvasSize)
 window.addEventListener('resize', setCanvasSize)
@@ -45,4 +49,38 @@ function startGame(){    //Funcion realiza los calculos para que responsivamente
     //         game.fillText(emojis[map[row - 1][col - 1]], elementsSize * col, elementsSize * row) 
     //     } 
     // }
+}
+
+window.addEventListener('keydown', moveByKeys)
+btnUp.addEventListener('click', moveUp)
+btnLeft.addEventListener('click', moveLeft)
+btnRight.addEventListener('click', moveRight)
+btnDown.addEventListener('click', moveDown)
+
+function moveByKeys(){
+    if (event.key == 'ArrowUp'){
+        moveUp()
+    } else if (event.key == 'ArrowLeft'){
+        moveLeft()
+    } else if (event.key == 'ArrowRight'){
+        moveRight()
+    } else if (event.key == 'ArrowDown'){
+        moveDown()
+    }
+}
+
+function moveUp(){
+    console.log('Hola arriba')
+}
+
+function moveLeft(){
+    console.log('Hola izquierda')
+}
+
+function moveRight(){
+    console.log('Hola derecha')
+}
+
+function moveDown(){
+    console.log('Hola abajo')
 }
