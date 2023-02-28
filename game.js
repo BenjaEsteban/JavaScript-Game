82acf5cd744b7fd5b1e6fca4d36399ba070a9b65
Benjamin Rodriguez <b.esteban.r.v@gmail.com>
2023-02-27 23:48:51 -0300

    Se agregaron funciones de detección y desplazamiento dentro del canvas

--- game.js
+++ game.js
@@ -12,6 +12,12 @@ window.addEventListener('resize', setCanvasSize)
 let canvasSize
 let elementsSize
 
+//Objeto que almacena las posiciones en el canvas
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+}
+
 function setCanvasSize(){
     if (window.innerHeight > window.innerWidth){
         canvasSize = window.innerWidth * 0.8
@@ -33,23 +39,33 @@ function startGame(){    //Funcion realiza los calculos para que responsivamente
 
     const map = maps[0].match(/[IXO\-]+/g).map(a => a.split(""))
 
+    game.clearRect(0, 0, canvasSize, canvasSize)
+
     map.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col]
             const posX = elementsSize * (colI + 1)
             const posY = elementsSize * (rowI + 1)
+
+            if (col == 'O'){
+                if (!playerPosition.x && !playerPosition.y){
+                    playerPosition.x = posX
+                    playerPosition.y = posY
+                    console.log({playerPosition})
+                }
+            }
             game.fillText(emoji, posX, posY)
         })
+        movePlayer()
     })
-
-    //OTRA MANERA DE CREAR EL MAPA
-
-    // for (let row = 1; row <= 10; row++) {  
-    //     for (let col = 1; col <= 10; col++){
-    //         game.fillText(emojis[map[row - 1][col - 1]], elementsSize * col, elementsSize * row) 
-    //     } 
-    // }
 }
+
+function movePlayer(){
+    game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
+
+}
+
+// Asignación de eventos para los botones y teclas de movimiento
 
 window.addEventListener('keydown', moveByKeys)
 btnUp.addEventListener('click', moveUp)
@@ -70,17 +86,44 @@ function moveByKeys(){
 }
 
 function moveUp(){
-    console.log('Hola arriba')
+    //console.log('Hola arriba')
+
+    if ((playerPosition.y - elementsSize) < elementsSize){
+        console.log('out')
+    } else{
+        playerPosition.y -= elementsSize
+        startGame()
+    }
 }
 
 function moveLeft(){
-    console.log('Hola izquierda')
+    //console.log('Hola izquierda')
+    
+    if ((playerPosition.x - elementsSize) < elementsSize){
+        console.log('out')
+    } else{
+        playerPosition.x -= elementsSize
+        startGame()
+    }
+   
 }
 
 function moveRight(){
-    console.log('Hola derecha')
+    //console.log('Hola derecha')
+    if ((playerPosition.x + elementsSize) > canvasSize){
+        console.log('out')
+    } else{
+        playerPosition.x += elementsSize
+        startGame()
+    }
 }
 
 function moveDown(){
-    console.log('Hola abajo')
+    //console.log('Hola abajo')
+    if ((playerPosition.y + elementsSize) > canvasSize){
+        console.log('out')
+    } else{
+        playerPosition.y += elementsSize
+        startGame()
+    }
 }
